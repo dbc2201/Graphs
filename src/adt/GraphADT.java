986241487b1f55c6c3e91cd8267a8681d0917868
dbc2201 <divyanshb22@gmail.com>
2020-04-27 10:03:43 +0530

    add to JavaDoc for GraphADT

--- src/adt/GraphADT.java
+++ src/adt/GraphADT.java
@@ -3,6 +3,8 @@ package adt;
 /**
  * This is the ADT for a Graph.
  * Methods deemed necessary for a Graph will be defined here first.
+ * A Graph is a pair of set of vertices, and a set of edges between a pair of vertices.
+ * We represent vertices in a Graph with integers from 0 to n-1.
  */
 public interface GraphADT {
 
